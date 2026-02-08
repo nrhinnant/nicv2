87b25f292d3292ed5a4aafcf2b01cdc2c1c54e66
Neil Hinnant <nrhinnant@gmail.com>
2026-02-08 10:11:57 -0800

    commit minor changes

--- src/shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ src/shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5858a0fe9ef1492c334e16a6606dc1ad6aedc3ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d971f27c379c3ae7c5eb66cffdf3b74595e0fc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
